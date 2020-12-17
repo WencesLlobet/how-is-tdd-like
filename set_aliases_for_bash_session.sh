506bbd1ebf40454cbf792842d9e576897bf527c5
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-17 10:47:35 +0100

    🟢🟢🟢🟢 GREEN - fixed bug on some shells: defining function based on alias 'gwip' - to squash

--- set_aliases_for_bash_session.sh
+++ set_aliases_for_bash_session.sh
@@ -1,9 +1,9 @@
-gwip() {
+function gwip() {
         git add -A; git commit -m "🟢🟢🟢🟢 GREEN - $1 - to squash"
 }
-rwip() {
+function rwip() {
         git add -A; git commit -m "🔴🔴🔴🔴 RED - $1 - to squash"
 }
-fwip() {
+function fwip() {
         git add -A; git commit -m "👍👍👍👍 REFACTOR - $1 - to squash"
 }
