9c6a2a8ad0e42feef829360bb5c20539c2540b7b
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-17 11:20:29 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- set_aliases_for_bash_session.sh
+++ set_aliases_for_bash_session.sh
@@ -1,9 +1,9 @@
-function gwip() {
+function gwip {
         git add -A; git commit -m "🟢🟢🟢🟢 GREEN - $1 - to squash"
 }
-function rwip() {
+function rwip {
         git add -A; git commit -m "🔴🔴🔴🔴 RED - $1 - to squash"
 }
-function fwip() {
+function fwip {
         git add -A; git commit -m "👍👍👍👍 REFACTOR - $1 - to squash"
 }
